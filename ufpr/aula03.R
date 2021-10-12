e30254672d2b403817e6b1890f1779a5427b9ae2
Eduardo Mariot <eduardo.mariot@gmail.com>
2021-10-11 23:55:26 -0300

    Add files via upload

--- ufpr/aula03.R
+++ ufpr/aula03.R
@@ -43,4 +43,64 @@ m7 = cbind(m7, log(m7[,3]))
 plot(m7[,1],m7[,4], lwd=2, type='b', col='tomato', main='Diagrama2')
 colnames(m6) = c("x","y","d","f","log")
 
-# frame 1:05
+# Multiplicação de matrizes 
+x = c(10, 13, 26, 46, 60, 78, 99)
+y = c(120, 160, 260, 133, 205, 310, 400)
+z = c(2, 2, 2, 2, 2, 2, 2)
+w = c(1, 3, 5, 8, 7, 12, 10)
+
+m1 = cbind(x,y,z,w) 
+m2 = rbind(x,y,z,w)
+dim(m1)
+dim(m2)
+m1 %*% m2 # Produto interno de matrizes (dot or inner product)
+t(m1) # Transposta
+t(m2)
+m1 * t(m2) # Produto da transposta (multiplicação simples)
+m1 %o% m2 # Produto externo de uma matriz (outer product)
+
+m3 = matrix(c(1,2,3,0,2,1,1,2,1), nrow=3,ncol=3)
+det(m3) # Determinante da matriz
+m3_i = solve(m3)# Inversa da matriz *determinante != 0
+round(m3 %*% m3_i) # uma matriz multiplicada pela sua inversa resulta em uma matriz unitária
+round(m3 * m3_i)
+
+## Arrays: são objetos parecidos com matrizes, mas com 3 ou mais dimensões
+x = 1:18 # array(linha, coluna, 3ªdimensao)
+a1 = array(x,dim=c(3,2,3))
+dim(a1)
+
+# runif: distribuição aleatória uniforme
+x = round(runif(200, min=0, max=99),0)
+a2 = array(x, dim=c(10,4,5))
+a2
+
+dimnames(a2)[[1]] = paste0(2000:2009)
+dimnames(a2)[[2]] = paste0('v',1:4)
+dimnames(a2)[[3]] = paste0("Cidade ", 1:5)
+
+a2[1,,] # Só o ano de 2000 de cada cidade
+a2[,1,] # Só a variável v1 de todas as cidades
+a2[,,1] # Só os dados da cidade 1
+
+## BoxPlot da variável 1, para todos os anos, de cada cidade
+boxplot(a2[,1,1:5],ylab='v1',main="Boxplot -v1 por cidade", col=2:6)
+a2[,1,1:5]
+
+# Data Frames
+df = as.data.frame(a2)
+df
+
+library(reshape2)
+df2 = melt(a2)
+colnames(df2) = c("Ano", "Var", "Cidade", "Valor")
+
+# Lendo data frames
+df2$Ano
+par(mfrow=c(1,2))
+plot(df2$Valor)
+plot(a2)
+
+df2[df2$Cidade=="Cidade 1",]
+df2[df2$Ano==2009 & df2$Var=='v2',4]
+barplot(df2[df2$Ano==2009 & df2$Var=='v2',4],col='yellow',xlab='Cidades')
